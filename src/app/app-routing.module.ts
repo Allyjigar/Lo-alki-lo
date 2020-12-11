1c4b875ba8609a8ecf5a7d8ade0456aedd170633
MauroGomez392 <mauro.gomez.gad@gmail.com>
2020-12-11 08:16:34 -0300

    rutas y myproducts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileLayoutComponent } from './layout/profile-layout/profile-layout.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { AnuncioComponent } from './pages/home/anuncio/anuncio.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MyFavoritesComponent } from './pages/profile/my-favorites/my-favorites.component';
+import { MyProductsComponent } from './pages/profile/my-products/my-products.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SubirProductoComponent } from './pages/subir-producto/subir-producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "myfavorites", component: MyFavoritesComponent},
+  {path: "myproducts", component: MyProductsComponent},
+  {path: "subirproducto", component: SubirProductoComponent},
+  {path: "anuncio", component: AnuncioComponent},
+  {path: "home", component: HomeComponent},
+  {path: "profile", component: ProfileComponent},
+  {path: "chat", component: ChatComponent},
+  {path: "auth", component: AuthComponent},
+  {path: "login", component: LoginComponent},
+  {path: "register", component: RegisterComponent},
+  {path:"profileLayout", component: ProfileLayoutComponent},
+  {path: "**", component: HomeComponent}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/profile/my-products/my-products.component.ts
+++ src/app/pages/profile/my-products/my-products.component.ts
@@ -6,9 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-products.component.css']
 })
 export class MyProductsComponent implements OnInit {
-
+  public isHidden: boolean = true;
+  public isHidden2: boolean = true;
   constructor() { }
-
+  mostrarMisProductos() {
+    if (this.isHidden == true) {
+      this.isHidden = false;
+      this.isHidden2 = true;
+    } else {
+      this.isHidden = true;
+      this.isHidden2 = true;
+    }
+  }
+  mostrarOtrosProductos() {
+    if (this.isHidden2 == true) {
+      this.isHidden2 = false;
+      this.isHidden = true;
+    } else {
+      this.isHidden = true;
+      this.isHidden2 = true;
+    }
+  }
   ngOnInit(): void {
   }
 
