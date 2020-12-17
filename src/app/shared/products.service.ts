99eae9faff539ad2f5fc7070d76ca8466fc7e4d8
MauroGomez392 <mauro.gomez.gad@gmail.com>
2020-12-17 09:44:03 -0300

    commiteo control

--- src/app/shared/products.service.ts
+++ src/app/shared/products.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Products } from '../models/products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  private url = "http://localhost:9191/products";
+  public product : Products;
+  
+  constructor(private http : HttpClient) { }
 
-  constructor() { }
+  
 }
