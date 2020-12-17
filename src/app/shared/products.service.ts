fdb7bd551cc7599bb8b8dbf4767418366f175fa6
MauroGomez392 <mauro.gomez.gad@gmail.com>
2020-12-17 10:35:36 -0300

    product-http

--- src/app/shared/products.service.ts
+++ src/app/shared/products.service.ts
@@ -11,5 +11,17 @@ export class ProductsService {
   
   constructor(private http : HttpClient) { }
 
-  
+  getProduct(id : string) {
+    return this.http.get(this.url + "?id=" + id); 
+  }
+  postProduct(newProduct : Products) {
+    return this.http.post(this.url, newProduct);
+  }
+  putProduct(newProduct : Products) {
+    return this.http.put(this.url, newProduct);
+  }
+  deleteProduct(id: number) {
+    let options = {headers: new HttpHeaders({'Content-Type': 'application/json'}), body : {id : id}};
+    return this.http.delete(this.url, options);
+  }
 }
